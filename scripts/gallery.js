f11e35050ed3aac9aa26bfaaab06bff78264f103
raagnaar07 <prantorumi07@gmail.com>
2025-09-22 01:33:26 +0200

    gallery.js

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,18 +1,8 @@
-const gallery = document.getElementById('gallery');
+function openPhoto(src) {
+  document.getElementById('viewer-img').src = src;
+  document.getElementById('photo-viewer').classList.remove('hidden');
+}
 
-const imageList = [
-  'DSC05936.jpg',
-  'DSC09107.jpg',
-  'DSC09544.jpg',
-  'DSC09866.jpg',
-  'DSC05837.jpg',
-  'DSC05838.jpg',
-  'DSC05840.jpg'
-];
-
-imageList.forEach(filename => {
-  const img = document.createElement('img');
-  img.src = `images/${filename}`;
-  img.alt = filename;
-  gallery.appendChild(img);
-});
+function closeViewer() {
+  document.getElementById('photo-viewer').classList.add('hidden');
+}
